830375d67d516fabc5d6d9deec75fbff2ee80dbb
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2023-11-30 12:46:46 +0600

    Bulk changes and project requierments added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,10 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
 
-  validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  ROLES = %w[admin user].freeze
-
-  def admin?
-    role == 'admin'
-  end
-
-  def self.recent_posts(user)
+  def self.recent_posts(id)
+    user = User.find_by(id:)
     user.posts.order(created_at: :desc).limit(3)
   end
 end
