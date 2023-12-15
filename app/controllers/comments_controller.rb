17c3949b4144e46b07db47cd0af56344955c0a7c
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2023-12-15 14:27:19 +0600

    comment controllers updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,10 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
-
   def create
     @user = current_user
     @comment = @user.comments.build(comment_params)
     @comment.post_id = params[:post_id]
-
     if @comment.save
       flash[:success] = 'Comment created successfully!'
     else
@@ -16,15 +14,17 @@ class CommentsController < ApplicationController
     end
     redirect_to user_post_path(current_user, @comment.post_id)
   end
-
   def destroy
     @comment = Comment.find(params[:id])
     authorize! :destroy, @comment
-    @comment.destroy
+    redirect_url = request.referer || fallback_url
+    if @comment.destroy
+      redirect_to redirect_url, notice: 'Comment was successfully deleted.'
+    else
+      redirect_to redirect_url, alert: 'Failed to delete the comment.'
+    end
   end
-
   private
-
   def comment_params
     params.require(:comment).permit(:text)
   end
